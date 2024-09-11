482b939f4cf536a29e9a3c1c33c3a71bc83a0528
kouhao@unicloud.com <kouhao@unicloud.com>
2024-09-11 18:12:01 +0800

    20240911 - init

--- bootstrap/route.go
+++ bootstrap/route.go
@@ -1,1 +1,68 @@
 package bootstrap
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"time"
+)
+
+type Route struct {
+	TargetHost   string
+	MatchPath    string
+	ServiceState string
+}
+
+func (r *Route) GetRouteMatchPath() string {
+	if len(r.MatchPath) == 0 {
+		return ""
+	}
+	return r.MatchPath
+}
+
+func (r *Route) GetRouteTargetHost() string {
+	if len(r.TargetHost) == 0 {
+		return ""
+	}
+	return r.TargetHost
+}
+
+func BuildRoute(routes []Route, r gin.Engine) {
+	routeMap := make(map[string]string)
+	if routes != nil {
+		for _, route := range routes {
+			routePath := route.GetRouteMatchPath()
+			routeTarget := route.GetRouteTargetHost()
+
+			if len(routePath) == 0 || len(routeTarget) == 0 {
+				continue
+			}
+
+			relativePath := routePath + "/*name"
+
+			target := routeMap[routePath]
+			if len(target) > 0 {
+				continue
+			}
+
+			routeMap[routePath] = routeTarget
+			r.Any(relativePath, func(c *gin.Context) {
+				proxyUrl, _ := url.Parse(routeTarget)
+
+				proxy := httputil.NewSingleHostReverseProxy(proxyUrl)
+				proxy.Transport = &http.Transport{
+					// 响应超时时间
+					ResponseHeaderTimeout: time.Duration(120) * time.Second,
+				}
+
+				proxy.ErrorHandler = func(writer http.ResponseWriter, request *http.Request, err error) {
+					// 正常响应502
+					writer.WriteHeader(http.StatusBadGateway)
+				}
+
+				proxy.ServeHTTP(c.Writer, c.Request)
+			})
+		}
+	}
+}
---
+++ go.mod
@@ -0,0 +1,28 @@
+module uca-center-gateway
+
+go 1.17
+
+require github.com/gin-gonic/gin v1.7.7
+
+require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f
+	github.com/mattn/go-colorable v0.1.13
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
+	github.com/robfig/cron/v3 v3.0.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+	gorm.io/driver/mysql v1.5.2
+	gorm.io/gorm v1.25.2-0.20230530020048-26663ab9bf55
+)
